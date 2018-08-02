cb11d622a01b5fb3f3872e0c0fa214b925c2f477
moijes12 <moijes12@gmail.com>
2018-08-02 07:16:08 -0700

    Added solution to Exercise 2.2

---
+++ chapter2/make_points_and_segments.scm
@@ -0,0 +1,35 @@
+;; Ex2.2
+
+(define make-point cons)
+(define x-point car)
+(define y-point cdr)
+
+(define make-segment cons)
+(define start-segment car)
+(define end-segment cdr)
+
+(define (midpoint segment)
+    (let ((average (lambda (x y) (/ (+ x y) 2.0))))
+        (make-point
+            (average (x-point (start-segment segment)) (x-point (end-segment segment)))
+            (average (y-point (start-segment segment)) (y-point (end-segment segment)))
+        )
+    )
+)
+
+(define (print-point p)
+    (newline)
+    (display "(")
+    (display (x-point p))
+    (display ",")
+    (display (y-point p))
+    (display ")")
+)
+
+(define start (make-point 2 3))
+(define end (make-point 4 6))
+
+(print-point start)
+(print-point end)
+(print-point (midpoint (make-segment start end)))
+
